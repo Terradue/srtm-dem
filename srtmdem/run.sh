87e0f3461cb41b01761f9fe4d5b8cdf80d2887d7
Fabrice Brito <fabrice.brito@terradue.com>
2014-02-25 12:45:08 +0100

    Update run.sh

--- srtmdem/run.sh
+++ srtmdem/run.sh
@@ -5,21 +5,20 @@ source ${ciop_job_include}
 
 # define the exit codes
 SUCCESS=0
-ERR_NOINPUT=1
 ERR_INVALIDFORMAT=2
-ERR_NOPARAMS=5
-ERR_JPEGTMP=7
-ERR_BROWSE=9
- 
+ERR_NOIDENTIFIER=5
+ERR_NODEM=7
+
 # add a trap to exit gracefully
 function cleanExit ()
 {
 local retval=$?
 local msg=""
 case "$retval" in
-$SUCCESS) msg="Processing successfully concluded";;
+$SUCCESS)           msg="Processing successfully concluded";;
 $ERR_INVALIDFORMAT) msg="Invalid format must be roi_pac or gamma";;
-$ERR_GDAL) msg="Graph processing of job ${JOBNAME} failed (exit code $res)";;
+$ERR_NOIDENTIFIER)  msg="Could not retrieve the dataset identifier";;
+$ERR_NODEM)         msg="DEM not generated";;
 *) msg="Unknown error";;
 esac
 [ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
@@ -31,9 +30,10 @@ export PATH=/application/srtmdem/bin:$PATH
 
 export DISPLAY=:1.0
 
-export MPLCONFIGDIR=/tmp/ #.matplotlib
+# SRTM.py uses matplotlib, set a temporary directory
+export MPLCONFIGDIR=$TMPDIR/
  
-# retrieve the parameters value from workflow or job default value
+# retrieve the DEM format to generate
 format="`ciop-getparam format`"
 
 case $format in
@@ -46,23 +46,37 @@ case $format in
 esac
 
 cd $TMPDIR
-set -x
+
+# read the catalogue reference to the dataset
 while read inputfile
 do
+  # the centroid R script get the WKT footprint and calculates the geometry centroid
   pts=`centroid $inputfile`
   lon=`echo $pts | cut -d " " -f 1`
   lat=`echo $pts | cut -d " " -f 2`
 
+  # use the dataset identifier as filename for the result
+  # SRTM.py concatenates .dem.<extension>
   ciop-log "INFO" "`basename $pts` centroid is ($lon $lat)" 
-  dem=`ciop-casmeta -f "dc:identifier" $inputfile`
+  dem_name=`ciop-casmeta -f "dc:identifier" $inputfile`
+  [ -z "$dem_name" ] && exit $ERR_NOIDENTIFIER 
  
+  # invoke the SRTM.py
+  # the folder /application/SRTM/data contains the SRTM tiles in tif format
   ciop-log "INFO" "Generating DEM"
-  SRTM.py $lat $lon $TMPDIR/$dem -D /application/SRTM/data/ $option
+  SRTM.py $lat $lon $TMPDIR/$dem_name -D /application/SRTM/data/ $option 1>&2
 
+  # check the output
+  [ ! -e $TMPDIR/$dem_name.dem ] && exit $ERR_NODEM
+
+  # save the bandwidth 
   ciop-log "INFO" "Compressing DEM"
-  tar cfz $dem.dem.tgz $dem*   
+  tar cfz $dem_name.dem.tgz $dem_name*   
 
+  # have the compressed archive published and its reference exposed as metalink
   ciop-log "INFO" "Publishing results"
-  ciop-publish -m $TMPDIR/$dem.dem.tgz #$TMPDIR 
-  rm -fr $dem*
+  ciop-publish -m $TMPDIR/$dem_name.dem.tgz  
+  
+  # clean-up for the next dataset reference
+  rm -fr $dem_name*
 done
