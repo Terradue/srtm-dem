11bf6c9ce7922d5aa2097d8fcfdf1d0d2822d329
fbarchetta <fbarchetta@sb-10-16-10-38.dev.terradue.int>
2015-04-17 16:14:39 +0200

    Aligning code with dem.terradue.int

--- src/main/app-resources/srtmdem/run.sh
+++ src/main/app-resources/srtmdem/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 # source the ciop functions (e.g. ciop-log)
 source ${ciop_job_include}
 
@@ -25,9 +25,9 @@ esac
 exit $retval
 }
 trap cleanExit EXIT
- 
 
-export PATH=/application/srtmdem/bin:$PATH 
+
+export PATH=/application/srtmdem/bin:$PATH
 
 export DISPLAY=:1.0
 
@@ -48,35 +48,43 @@ esac
 # read the catalogue reference to the dataset
 while read inputfile
 do
+   printenv
    UUIDTMP="/tmp/`uuidgen`"
    mkdir $UUIDTMP
    cd $UUIDTMP
 
+   ciop-log "DEBUG" "inputfile before $inputfile"
+   #inputfile=$( opensearch-client "$inputfile" enclosure | tail -1 )
+   ciop-log "DEBUG" "inputfile after $inputfile"
+
    # SRTM.py uses matplotlib, set a temporary directory
    export MPLCONFIGDIR=$UUIDTMP/
 
-   ciop-log "INFO" "Working on $inputfile in $UUIDTMP" 
+   ciop-log "INFO" "Working on $inputfile in $UUIDTMP"
 
-   dem_name=`ciop-casmeta -f "dc:identifier" "$inputfile"`
-   [ -z "$dem_name" ] && exit $ERR_NOIDENTIFIER 
+   #dem_name=`ciop-casmeta -f "dc:identifier" "$inputfile"`
+   dem_name=`uuidgen`
+   [ -z "$dem_name" ] && exit $ERR_NOIDENTIFIER
 
+   wkt="$( opensearch-client "$inputfile" wkt | tail -1 )"
+   ciop-log "DEBUG" "wkt is $wkt"
    # the centroid R script get the WKT footprint and calculates the geometry centroid
-   pts=`centroid "$inputfile"`
+   pts=`centroid "$wkt"`
    lon=`echo $pts | cut -d " " -f 1`
    lat=`echo $pts | cut -d " " -f 2`
-
+ ciop-log "DEBUG" "centroid finished"
   # GMTSAR
   [ "$flag" == "true" ] && {
     # invoke make_dem.csh
 
 #   #recreate a 3 deg bbox around the centroid lon/lat
-#    lon1=$( echo "$lon - 1.5" | bc ) 
+#    lon1=$( echo "$lon - 1.5" | bc )
 #    lon2=$( echo "$lon + 1.5" | bc )
 #    lat1=$( echo "$lat - 1.5" | bc )
 #    lat2=$( echo "$lat + 1.5" | bc )
 
     #using the new mbr R script to generate the bbox to extend
-    bbox=$( mbr "$inputfile" )
+    bbox=$( mbr "$wkt" )
     lon1=$( echo "$( echo "$bbox" | cut -d "," -f 1 ) -1.5" | bc | cut -d "." -f 1 )
     lon2=$( echo "$( echo "$bbox" | cut -d "," -f 2 ) +1.5" | bc | cut -d "." -f 1 )
     lat1=$( echo "$( echo "$bbox" | cut -d "," -f 3 ) -1.5" | bc | cut -d "." -f 1 )
@@ -86,13 +94,13 @@ do
 
     export PATH=$PATH:/usr/local/GMTSAR/gmtsar/csh/
     /usr/local/GMTSAR/gmtsar/csh/make_dem.csh $lon1 $lon2 $lat1 $lat2 2 /data/SRTM3/World/
-    
+
     #rename the output
     #mv dem.grd $dem_name.grd
     #mv dem_grad.png $dem_name.png
     #mv dem_grad.kml $dem_name.kml
     [ ! -e $UUIDTMP/dem.grd ] && exit $ERR_NODEM
-    
+
      # save the bandwidth
      ciop-log "INFO" "Compressing DEM"
      tar cfz $dem_name.dem.tgz dem*
@@ -107,17 +115,17 @@ do
    ciop-log "INFO" "Generating DEM"
    SRTM.py $lat $lon $UUIDTMP/$dem_name -D /data/SRTM41/ $option 1>&2
    [ ! -e $UUIDTMP/$dem_name.dem ] && exit $ERR_NODEM
-  
+
    # save the bandwidth
    ciop-log "INFO" "Compressing DEM"
    tar cfz $dem_name.dem.tgz $dem_name*
-  } 
- 
+  }
+
   # have the compressed archive published and its reference exposed as metalink
   ciop-log "INFO" "Publishing results"
-  ciop-publish -m $UUIDTMP/$dem_name.dem.tgz  
-   
+  ciop-publish -m $UUIDTMP/$dem_name.dem.tgz
+
   # clean-up for the next dataset reference
   rm -fr $UUIDTMP
-   
+
 done
