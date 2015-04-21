8e54dc81e62f5e0239dac369cdd56ff748bc9199
Fabio D'Andria <fabio.dandria@terradue.com>
2015-04-21 10:31:34 +0200

    [srtm-dem] fixed postInstall scriplet

--- src/main/install/postinstall.sh
+++ src/main/install/postinstall.sh
@@ -8,12 +8,12 @@ cat <<EOF > /etc/init.d/Xvfb
 
 #chkconfig: 345 95 50
 #description: Starts xvfb on display 99
-if [ -z "$1" ]; then
-  echo "`basename $0` {start|stop}"
+if [ -z "\$1" ]; then
+  echo "service Xvfb {start|stop}"
   exit
 fi
 
-case "$1" in
+case "\$1" in
   start)
     /usr/bin/Xvfb :99 +extension RANDR -screen 0 1280x1024x24 &
   ;;
@@ -24,6 +24,7 @@ case "$1" in
 esac
 EOF
 
+chmod 700 /etc/init.d/Xvfb
 chkconfig --add Xvfb
 chkconfig Xvfb on
 
